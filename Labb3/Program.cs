57570ef028033e70cc354c2a02be17f384b08e55
Henks91 <98890612+Henks91@users.noreply.github.com>
2023-02-27 11:29:27 +0100

    Add files via upload

---
+++ Labb3/Program.cs
@@ -0,0 +1,25 @@
+﻿using DataLayer;
+using DataLayer.Context;
+using System;
+using DataLayer.Seed;
+using Business.Classes;
+using Microsoft.EntityFrameworkCore;
+using GUI;
+using System.Windows.Forms;
+
+namespace TestLabb2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+            
+            BokningSeed.Populate(bokningDbContext);
+            BokningSeed.Populate(bokningDbContext);
+
+
+        }
+    }
+}
