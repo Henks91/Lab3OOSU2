692613bde17b6f667cf00991ab665a37b83e769a
fredriklinh <98946093+fredriklinh@users.noreply.github.com>
2023-03-07 14:33:04 +0100

    Ny

--- PresentationsLager.WPF.MVVM/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
+++ PresentationsLager.WPF.MVVM/obj/Debug/netcoreapp3.1/Views/MainWindow.g.i.cs
@@ -102,7 +102,7 @@ namespace PresentationsLager.WPF.MVVM.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PresentationsLager.WPF.MVVM;V1.0.0.0;component/views/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PresentationsLager.WPF.MVVM;component/views/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Views\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
