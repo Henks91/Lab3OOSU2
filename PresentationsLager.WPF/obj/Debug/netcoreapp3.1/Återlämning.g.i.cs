f1ec29c7004311de1dd81e96c06bf955cab77a32
Daniel Noun <danielnoun21@gmail.se>
2023-03-08 10:41:15 +0100

    as

--- PresentationsLager.WPF/obj/Debug/netcoreapp3.1/Återlämning.g.i.cs
+++ PresentationsLager.WPF/obj/Debug/netcoreapp3.1/Återlämning.g.i.cs
@@ -47,13 +47,13 @@ namespace PresentationsLager.WPF {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/PresentationsLager.WPF;component/%c3%85terl%c3%a4mning.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/PresentationsLager.WPF;V1.0.0.0;component/%c3%85terl%c3%a4mning.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Återlämning.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -63,7 +63,7 @@ namespace PresentationsLager.WPF {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.17.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
